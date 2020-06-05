66b947ce067aff059e309ac990ad6d6cbcfc16e3
Kechi-web <65464063+Kechi-web@users.noreply.github.com>
2020-06-05 22:41:21 +0300

    adaptive slider

--- js/script.js
+++ js/script.js
@@ -7,14 +7,34 @@ $(document).ready(function () {
 			header.classList.add('header-dark')
 		}
 	});
+
 	$('.team__slider').slick({
-		infinite: true,
+		infinite: false,
 		slidesToShow: 3,
 		slidesToScroll: 3,
 		arrows: false,
 		dots: true,
-		speed: 1200
+		speed: 1200,
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				// infinite: true,
+				dots: true
+			  }
+			},
+			{
+			  breakpoint: 768,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			}
+		]
 	});
+
 	$('a[href^="#"]').click(function () {
 		var target = $(this).attr('href');
 		$('html, body').animate({
@@ -22,7 +42,7 @@ $(document).ready(function () {
 		}, 1200);
 		return false;
 	});
-	$('.burger').on('click',function (){
+	$('.burger').on('click', function () {
 		$(this).toggleClass('burger-act');
 		$('.header ul').toggleClass('open');
 	});
@@ -38,34 +58,34 @@ $(document).ready(function () {
 	});
 	$('#link-all').on('click', function () {
 		$('.works__tab').css({
-			'display' : 'none',
+			'display': 'none',
 		});
 		$('#tab-all').css({
-			'display' : 'block',
+			'display': 'block',
 		});
 	});
 	$('#link-web').click(function () {
 		$('.works__tab').css({
-			'display' : 'none',
+			'display': 'none',
 		});
 		$('#tab-web').css({
-			'display' : 'block',
+			'display': 'block',
 		});
 	});
 	$('#link-des').click(function () {
 		$('.works__tab').css({
-			'display' : 'none',
+			'display': 'none',
 		});
 		$('#tab-des').css({
-			'display' : 'block',
+			'display': 'block',
 		});
 	});
 	$('#link-moc').click(function () {
 		$('.works__tab').css({
-			'display' : 'none',
+			'display': 'none',
 		});
 		$('#tab-moc').css({
-			'display' : 'block',
+			'display': 'block',
 		});
 	});
 });
